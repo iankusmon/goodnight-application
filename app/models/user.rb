f6d60947a0123c1e0247b58fe38362cd358f081a
iankusmon <muh.rahardian.k@gmail.com>
2025-09-24 17:36:54 +0700

    feat: user model

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+
+class User < ApplicationRecord
+  has_many :sleep_sessions, dependent: :destroy
+  has_many :active_follows,  class_name: "Follow", foreign_key: :follower_id, dependent: :destroy
+  has_many :passive_follows, class_name: "Follow", foreign_key: :followed_id, dependent: :destroy
+
+  has_many :following, through: :active_follows, source: :followed
+  has_many :followers, through: :passive_follows, source: :follower
+
+  validates :name, presence: true
+end
